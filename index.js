164bc2958067b8eb38860931ca4a66948417b73e
zarrukh_jurakulov <zarrukhjurakulov474@gmail.com>
2023-06-10 14:17:12 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const getArgs = require("./helpers/args");
+
+const startCLI = () => {
+  const args = getArgs(process.argv);
+  if (args.h) {
+    // help
+  }
+  if (args.s) {
+    // save city
+  }
+  if (args.t) {
+    // save token
+  }
+  // result
+  console.log(args);
+};
+
+startCLI();
